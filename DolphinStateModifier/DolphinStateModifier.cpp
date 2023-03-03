909190da3666f33ef99c5722e60fc4a891d04d34
J <puffleboy101@hotmaIl.co.uk>
2023-03-03 19:06:19 +0000

    works

--- DolphinStateModifier/DolphinStateModifier.cpp
+++ DolphinStateModifier/DolphinStateModifier.cpp
@@ -4,6 +4,21 @@
 #include <iostream>
 #include <fstream>
 
+#define BUTTHURT_MAX 14
+#define BUTTHURT_MIN 0
+
+#define LEVEL1_THRESHOLD 0
+#define LEVEL2_THRESHOLD 300
+#define LEVEL3_THRESHOLD 1800
+
+typedef struct {
+	uint8_t magic;
+	uint8_t version;
+	uint8_t checksum;
+	uint8_t flags;
+	uint32_t timestamp;
+} SavedStructHeader;
+
 typedef struct {
 	uint8_t icounter_daily_limit[7];
 	uint8_t butthurt_daily_limit;
@@ -14,34 +29,86 @@ typedef struct {
 	uint64_t timestamp;
 } DolphinStoreData;
 
+int xpToLevel(int xp) {
+	if (xp >= LEVEL1_THRESHOLD && xp < LEVEL2_THRESHOLD) {
+		return 1;
+	}
+	else if (xp >= LEVEL2_THRESHOLD && xp < LEVEL3_THRESHOLD) {
+		return 2;
+	}
+	else if (xp >= LEVEL3_THRESHOLD) {
+		return 3;
+	}
+	else {
+		return -1;
+	}
+}
+
 int main(int argc, char* argv[])
 {
+	char* inputPath = 0;
 
+	int nextLevel = -1;
+	int nextButthurt = -1;
+
+	SavedStructHeader header = { 0 };
 	DolphinStoreData data = { 0 };
 	FILE* ddFile;
-	
-	if (argc < 2) {
-		// No arguments
-		// Print help
+
+	bool saveRequired = false;
+
+	for (int i = 0; i < argc; i++) {
+		if (strcmp(argv[i], "-f") == 0 && i + 1 <= argc && !inputPath) {
+			inputPath = argv[i + 1];
+		}
+
+		if (strcmp(argv[i], "-l") == 0 && i + 1 <= argc && nextLevel == -1) {
+			nextLevel = (int)strtol(argv[i + 1], NULL, 10);
+		}
+
+		if (strcmp(argv[i], "-b") == 0 && i + 1 <= argc && nextButthurt == -1) {
+			nextButthurt = (int)strtol(argv[i + 1], NULL, 10);
+		}
+	}
+
+	if (!inputPath) {
+		printf_s("Please specify an input path (-f)\n");
 		return 0;
 	}
 
-	if (strcmp(argv[1], "-r") == 0) {
-		// Read
-		// Read from file
-		fopen_s(&ddFile, ".dolphin.state", "rb");
-		if (!ddFile) { return;  }
-		fread_s(&data, sizeof(DolphinStoreData), sizeof(DolphinStoreData), 1, ddFile);
-		// Print to console
-		return 0;
+	fopen_s(&ddFile, inputPath, "r+b");
+	if (!ddFile) { return 0; }
+	fread_s(&header, sizeof(SavedStructHeader), sizeof(SavedStructHeader), 1, ddFile);
+	fread_s(&data, sizeof(data), sizeof(DolphinStoreData), 1, ddFile);
+
+	if (nextLevel >= LEVEL1_THRESHOLD) {
+		data.icounter = nextLevel;
+		saveRequired = true;
 	}
 
-	if (strcmp(argv[1], "-l") == 0) {
-		// Set level
+	if (nextButthurt >= BUTTHURT_MIN && nextButthurt < BUTTHURT_MAX) {
+		data.butthurt = nextButthurt;
+		saveRequired = true;
 	}
 
-	if (strcmp(argv[1], "-b") == 0) {
-		// Set butthurt
+	printf_s("Flags     : %i\n", data.flags);
+	printf_s("XP        : %i (LVL %i) %s\n", data.icounter, xpToLevel(data.icounter), nextLevel == -1 ? "" : "*");
+	printf_s("Butthurt  : %i %s\n", data.butthurt, nextButthurt == -1 ? "" : "*");
+	printf_s("Timestamp : %ld\n", (long)data.timestamp);
+
+	if (saveRequired) {
+		uint8_t checksum = 0;
+		uint8_t* source = (uint8_t*)&data;
+		for (size_t i = 0; i < sizeof(DolphinStoreData); i++) {
+			checksum += source[i];
+		}
+		header.checksum = checksum;
+
+		fseek(ddFile, 0, SEEK_SET);
+		fwrite(&header, sizeof(SavedStructHeader), 1, ddFile);
+		fwrite(&data, sizeof(data), 1, ddFile);
 	}
+
+	fclose(ddFile);
 }
 
